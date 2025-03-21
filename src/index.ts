52726c908878eb54eb20c30fc22b75e05b4fcff0
JasonNad86 <jmn10@mail.aub.edu>
2025-03-21 16:34:50 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ console.log(deepEqual({ a: { b: 2 } }, { a: { b: 2 } })); // true
 console.log(deepEqual({ a: { b: 2 } }, { a: { b: 3 } })); // false
 
 const cache = new LRUCache(2);
-cache.put(1, 1);  // Cache = {1=1}
+cache.put(1, 1);  
 cache.put(2, 2);  // Cache = {1=1, 2=2}
 console.log(cache.get(1));  // Returns 1, Cache = {2=2, 1=1}
 cache.put(3, 3);  // Evicts key 2, Cache = {1=1, 3=3}
